c26742e1ef42551723ffe435b3d2c1599484c320
SandyCT97 <sandy.pierre@consertotech.pro>
2018-08-10 14:06:06 +0200

    Add doc on state and nonce in helpers/utils.js

--- helpers/utils.js
+++ helpers/utils.js
@@ -4,8 +4,12 @@
  * @see @link{ https://partenaires.franceconnect.gouv.fr/fcp/fournisseur-service# }
  */
 import config from '../config/config.json';
-// TODO hard code state et nonce because they normaly generate from every request
-export const getAuthorizationUrl = () => `${config.AUTHORIZATION_URL}?response_type=code`
+/**
+ * Format the url 's that is used in a redirect call to France Connect Auhtorize endpoint.
+ * State and nonce value should be generated for every request.
+ * @returns {string}
+ */
+const getAuthorizationUrl = () => `${config.AUTHORIZATION_URL}?response_type=code`
     + `&client_id=${config.CLIENT_SECRET}&redirect_uri=${config.REDIRECT_URL}`
     + `&scope=${config.SCOPE}&state=customState11&nonce=customNonce11`;
 
@@ -14,5 +18,7 @@ export const getAuthorizationUrl = () => `${config.AUTHORIZATION_URL}?response_t
  * Format the url 's that is used in a redirect call to France Connect logout API endpoint
  * @returns {string}
  */
-export const getLogoutUrl = req => `${config.LOGOUT_URL}?id_token_hint=${req.session.id_token}`
+const getLogoutUrl = req => `${config.LOGOUT_URL}?id_token_hint=${req.session.id_token}`
   + `&state=customState11&post_logout_redirect_uri=${config.LOGOUT_REDIRECT_URL}`;
+
+export { getLogoutUrl, getAuthorizationUrl };
